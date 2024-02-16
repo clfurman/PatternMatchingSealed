a552fc3bf74701b94496e2e8f3031cb2f38dea96
Crystal Furman <crystal.furman@gmail.com>
2024-02-16 14:38:02 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,14 +1,14 @@
-
 public class DogTester
 {
     public static void main (String [] args)
     {
      System.out.println ("Let\'s simulate a vet office.");
-     Dog d = new Dog ("Brady", "Whippet", 39.6);
+     Dog d = new Dog (new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
      System.out.println (d);
-     
+
     }
 }
 
-record Dog (String name, String breed, double weight){}
+record Dog (LicenseName name, String breed, double weight){}
 
+record LicenseName (String showName, String nickName) {} 
