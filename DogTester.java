93bf613f97c4081acc52bbe8c6e1e7715e94f833
clfurman <crystal.furman@gmail.com>
2024-02-16 13:40:20 -0500

    add DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -4,35 +4,7 @@ public class DogTester
 {
     public static void main (String [] args)
     {
-     Dog d = new Dog (new LicenseName ("Brady the Blue Brindle", "Brady"), "Whippet", 46.7);
-     Cat c = new Cat ("Sweetie", "Siamese", 7.5);
-     ArrayList <Animal> animals = new ArrayList<Animal>();
-     animals.add(d);
-     animals.add(c);
-
-     for (Animal a: animals)
-     {
-        switch (a) {
-           case Dog (LicenseName n, _, _):
-            {
-             System.out.println (n.nickname());
-             break;
-            }
-           case Cat (String n, _, _):
-           {  System.out.println (n);
-              break;
-           }
-        }
-        
-     }
-
+     System.out.println ("Let\'s simulate a vet office.")
     }
 }
 
-sealed interface Animal permits Dog, Cat {}
-
-record Dog (LicenseName name, String Breed, double weight) implements Animal{}
-
-record Cat (String name, String Breed, double weight) implements Animal{}
-
-record LicenseName (String fullName, String nickname){}
