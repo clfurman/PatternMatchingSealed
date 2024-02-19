ff9d81c662c00c677eef660944345ce9a4c06d8a
clfurman <crystal.furman@gmail.com>
2024-02-19 13:01:44 -0500

    add DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,16 +1,24 @@
-
+import java.util.*;
 public class DogTester
 {
     public static void main (String [] args)
     {
-     System.out.println ("Let\'s simulate a vet office.");
-     Dog d = new Dog (new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
-     System.out.println (d);
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+
+        System.out.println ("Let\'s simulate a vet office.");
+        Dog d = new Dog (new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
+        Cat c = new Cat ("Mittens", "Siamese", 8.2);
+        animals.add (d);
+        animals.add (c);
+        System.out.println (animals);
 
     }
 }
 
-record Dog (LicenseName name, String breed, double weight){}
+record Dog (LicenseName name, String breed, double weight) implements Animal{}
+record Cat (String name, String breed, double weight) implements Animal{}
+
+sealed interface Animal permits Dog, Cat{}
 
 record LicenseName (String showName, String nickName) {} 
 
