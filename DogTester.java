2d411f3d04a5a43e56696549b9d5278788741a61
Crystal Furman <crystal.furman@gmail.com>
2024-02-27 11:12:24 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,10 +1,43 @@
 import java.util.*;
-
 public class DogTester
 {
     public static void main (String [] args)
     {
-     System.out.println ("Let\'s simulate a vet office.");
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+
+        System.out.println ("Let\'s simulate a vet office.");
+        Dog d = new Dog (new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
+        Cat c = new Cat ("Mittens", "Siamese", 8.2);
+        animals.add (d);
+        animals.add (c);
+        System.out.println (animals);
+
+        for (Animal a : animals)
+        {
+            switch (a)
+            {
+                //uses type matching to declare dog and assign it to (Dog)a
+                case Dog dog: 
+                {                
+                    System.out.println (dog.name().showName());
+                    break;
+                }
+                case Cat cat:
+                {
+                    System.out.println (cat.name());
+                    break;
+                }
+                //since Animal is sealed, no default is needed. The only cases are Dog and Cat
+            }
+        }
+
     }
 }
 
+record Dog (LicenseName name, String breed, double weight) implements Animal{}
+record Cat (String name, String breed, double weight) implements Animal{}
+
+//sealed interface means that only Dog and Cat can implement Animal
+sealed interface Animal permits Dog, Cat{}
+
+record LicenseName (String showName, String nickName) {} 
