97330120f5370e8fb76b08b7aa49036ec3820ee6
Crystal Furman <crystal.furman@gmail.com>
2024-03-01 08:23:16 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,8 +1,7 @@
 import java.util.*;
-public class DogTester
-{
-    public static void main (String [] args)
-    {
+
+public class DogTester {
+    public static void main (String [] args) {
         ArrayList<Animal> animals = new ArrayList<Animal>();
 
         System.out.println ("Let\'s simulate a vet office.");
@@ -12,28 +11,30 @@ public class DogTester
         animals.add (c);
         System.out.println (animals);
 
-        for (Animal a : animals)
-        {
+        for (Animal a : animals) {
             //Pattern matching used to declare and assign the variable dog to (Dog)a
-            if (a instanceof Dog dog)
-            {
+            if (a instanceof Dog dog) {
                 System.out.println (dog.name().showName() + " is a " + dog.breed());
             }
-            else if (a instanceof Cat cat)
-            {
+            else if (a instanceof Cat cat) {
                 System.out.println (cat.name() + " is a " + cat.breed());
             }
-           
         }
-
     }
 }
-
-record Dog (LicenseName name, String breed, double weight) implements Animal{}
-record Cat (String name, String breed, double weight) implements Animal{}
-
 //sealed interface means only Dog and Cat can implement Animal
-sealed interface Animal permits Dog, Cat{}
+sealed interface Animal permits Dog, Cat {
+}
 
-record LicenseName (String showName, String nickName) {} 
+record LicenseName (String showName, String nickName) {
+} 
+
+record Dog (LicenseName name, String breed, double weight) implements Animal {
+}
+
+record Cat (String name, String breed, double weight) implements Animal {
+}
+
+
+
 
