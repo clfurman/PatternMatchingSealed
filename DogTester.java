5a0a81ae9410dce26dfebfe2d6a8d4afaabca941
Crystal Furman <crystal.furman@gmail.com>
2024-03-01 08:26:07 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,8 +1,7 @@
 import java.util.*;
-public class DogTester
-{
-    public static void main (String [] args)
-    {
+
+public class DogTester {
+    public static void main (String [] args) {
         ArrayList<Animal> animals = new ArrayList<Animal>();
 
         System.out.println ("Let\'s simulate a vet office.");
@@ -12,33 +11,37 @@ public class DogTester
         animals.add (c);
         System.out.println (animals);
 
-        for (Animal a : animals)
-        {
-            switch (a)
-            {
+        for (Animal a : animals) {
+            switch (a) {
                 //since breed and weight are not being used, we don't need to store these values
-                case Dog (LicenseName name, _, _): 
-                {                
+                case Dog (LicenseName name, _, _): {                
                     System.out.println (name.showName());
                     break;
                 }
-                case Cat (String name, _, _):
-                {
+                case Cat (String name, _, _): {
                     System.out.println (name);
                     break;
                 }
                 //since Animal is sealed, no default is needed. The only cases are Dog and Cat
             }
         }
-
     }
 }
 
-record Dog (LicenseName name, String breed, double weight) implements Animal{}
-record Cat (String name, String breed, double weight) implements Animal{}
-
 //sealed interface means that only Dog and Cat can implement Animal
-sealed interface Animal permits Dog, Cat{}
+sealed interface Animal permits Dog, Cat {
+}
 
-record LicenseName (String showName, String nickName) {} 
+record LicenseName (String showName, String nickName) {
+} 
+
+record Dog (LicenseName name, String breed, double weight) implements Animal {
+}
+
+record Cat (String name, String breed, double weight) implements Animal {
+}
+
+
+
+
 
