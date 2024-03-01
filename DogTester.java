4dd0dbf56cdebe0bac2f50ee2b598ea3b5ba92ef
Crystal Furman <crystal.furman@gmail.com>
2024-03-01 08:17:27 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,10 +1,8 @@
 import java.util.*;
 
-public class DogTester
-{
-    public static void main (String [] args)
-    {
-    System.out.println ("Let\'s simulate a vet office.");
+public class DogTester {
+    public static void main (String [] args) {
+       System.out.println ("Let\'s simulate a vet office.");
     }
 }
 
