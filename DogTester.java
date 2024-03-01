935f4f8c4dcaee59d2d8862d85e9fafc2fe76679
Crystal Furman <crystal.furman@gmail.com>
2024-03-01 08:27:39 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,33 +1,34 @@
 import java.util.*;
 
-public class DogTester
-{
-    public static void main (String [] args)
-    {
-     Dog d = new Dog (new LicenseName ("Brady the Blue Brindle", "Brady"), "Whippet", 46.7);
-     Cat c = new Cat ("Sweetie", "Siamese", 7.5);
-     ArrayList <Animal> animals = new ArrayList<>();
-     animals.add(d);
-     animals.add(c);
+public class DogTester {
+    public static void main (String [] args) {
+        Dog d = new Dog (new LicenseName ("Brady the Blue Brindle", "Brady"), "Whippet", 46.7);
+        Cat c = new Cat ("Sweetie", "Siamese", 7.5);
+        ArrayList <Animal> animals = new ArrayList<>();
+        animals.add(d);
+        animals.add(c);
 
-     for (Animal a: animals)
-     {
-        switch (a) {
-           case Dog (LicenseName n, _, _)->System.out.println(n.nickname());
-           
-           case Cat (String n, _, _)->System.out.println(n);
+        for (Animal a: animals){
+            switch (a) {
+                case Dog (LicenseName n, _, _)->System.out.println(n.nickname());
+                
+                case Cat (String n, _, _)->System.out.println(n);
+            } 
         }
-        
-     }
-
     }
 }
 
-sealed interface Animal permits Dog, Cat {}
+sealed interface Animal permits Dog, Cat {
+}
 
-record Dog (LicenseName name, String Breed, double weight) implements Animal{}
+record LicenseName (String fullName, String nickname) {
+}
 
-record Cat (String name, String Breed, double weight) implements Animal{}
+record Dog (LicenseName name, String Breed, double weight) implements Animal {
+}
 
-record LicenseName (String fullName, String nickname){}
+record Cat (String name, String Breed, double weight) implements Animal {
+}
+
+
 
